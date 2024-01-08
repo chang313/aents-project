f60d9d7fb6945b6632c3d970e749c19d4b724894
Chang313 <pch313@kaist.ac.kr>
2024-01-09 04:19:15 +0900

    pass clicked ArticleCard's data when routing

--- app/components/ArticleCard.tsx
+++ app/components/ArticleCard.tsx
@@ -1,27 +1,40 @@
 import { Container, Typography, Card, CardContent, CardMedia, Grid } from '@mui/material';
 import { useRouter } from 'next/router';
-import { createSlug } from '../_lib/util';
+import { createSlug, datetime2YearMonthDay } from '../_lib/util';
 import React from 'react';
 import Image from 'next/image';
+import { LINK_ARTICLES, LINK_MY_PAGE } from '../_consts/Links';
+import { ImageBaseUrl } from '../_consts/Links';
 
 
 
-export default function ArticleCard({title, content, writer_name, image, priority, isEditable}) {
+export default function ArticleCard({id, title, content, writer_name, image, date, priority, isEditable}) {
   const router = useRouter();
-  const ImageBaseUrl = '/_f/articles/images/'
+  // const ImageBaseUrl = '/_f/articles/images/'
+
+  const korYearMonthDay = datetime2YearMonthDay(date);
+  
 
   const handleCardClick = () => {
     // Navigate to the target URL when the card is clicked
   
     let url;
     if (isEditable) {
-      url = '/my-page/' + createSlug(title);
+      url = LINK_MY_PAGE + '/' + createSlug(title);
+
+      // router.push(url);
+      const data = {id, title, content, image, date };
+      router.push({
+        pathname: url,
+        query: { data: JSON.stringify(data) },
+      });
     } else {
-      url = '/articles/' + createSlug(title);
+      url = LINK_ARTICLES + '/' + createSlug(title);
+      router.push(url);
     }
     
     console.log(url)
-    router.push(url);
+    
   };
 
   return (
@@ -48,15 +61,18 @@ export default function ArticleCard({title, content, writer_name, image, priorit
           {/* Title on the right */}
           <Grid item xs={12} sm={6}>
             <CardContent>
-              <Typography variant="h5" component="div">
+              <Typography variant="h4" component="div">
                 {title}
               </Typography>
-              <Typography variant="body2" color="text.secondary">
+              {/* <Typography variant="body2" color="text.secondary">
                 {content}
-              </Typography>
+              </Typography> */}
               <Typography variant="body2" color="text.secondary">
                 Author: {writer_name}
               </Typography>
+              {/* <Typography>
+                {korYearMonthDay}
+              </Typography> */}
 
             </CardContent>
           </Grid>
